3f80f56c1ede115556d82e34775046a72d9d12e8
Sahithi <sahithi1722@gmail.com>
2026-07-23 00:33:15 +0530

    Final deployment

--- cdas/settings.py
+++ cdas/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'cdas.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "cdas_db",
+        "USER": "root",
+        "PASSWORD": "Mysql@123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 

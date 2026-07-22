42ad9fc26a1ef23a032a999bbde8f5107544b231
Sahithi <sahithi1722@gmail.com>
2026-07-22 22:40:12 +0530

    Prepare for deployment

--- cdas/settings.py
+++ cdas/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-s+70w#tqx#u19u+@5=9o_qa0b9*61$3bqt57)!%^4_q%ex&_iw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
